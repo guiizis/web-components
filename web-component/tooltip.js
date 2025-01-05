823fe9388fb6389abde6d0130c46fa6c1b3d3ad0
guiziis <guigamoda@hotmail.com>
2025-01-05 13:27:53 -0300

    getAttributes

--- web-component/tooltip.js
+++ web-component/tooltip.js
@@ -1,14 +1,27 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super()
-
+    this._toolTipContainer
+    this._toolTipText = 'no tooltip text'
   }
 
   connectedCallback() { //WebComponent lifecycle method
+    this._toolTipText = this.getAttribute('text')
     const toolTipIcon = document.createElement('span')
-    toolTipIcon.textContent = '(?)'
-    
+    toolTipIcon.textContent = ' (?)'
+    toolTipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+    toolTipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
     this.appendChild(toolTipIcon)
+  }
+
+  _showTooltip() {
+    this._toolTipContainer = document.createElement('div')
+    this._toolTipContainer.textContent = this._toolTipText
+    this.appendChild(this._toolTipContainer)
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._toolTipContainer)
   }
 
 }
