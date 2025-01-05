418aba514644b3ed3f1ff7cb14bb2c4348baab88
guiziis <guigamoda@hotmail.com>
2025-01-05 13:10:43 -0300

    lifecycles

--- web-component/tooltip.js
+++ web-component/tooltip.js
@@ -1,8 +1,13 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super()
+
+  }
+
+  connectedCallback() { //WebComponent lifecycle method
     const toolTipIcon = document.createElement('span')
     toolTipIcon.textContent = '(?)'
+    
     this.appendChild(toolTipIcon)
   }
 
