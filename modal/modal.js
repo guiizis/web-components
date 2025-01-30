1634195b47093911e5d4a6e32ca5ee8682131807
guiziis <guigamoda@hotmail.com>
2025-01-30 20:58:04 -0300

    set modal opacity to zero

--- modal/modal.js
+++ modal/modal.js
@@ -12,6 +12,8 @@ class Modal extends HTMLElement {
           height: 100vh;
           background: rgba(0, 0, 0, 0.75);
           z-index: 10;
+          opacity: 0;
+          pointer-events: none;
         }
 
         #modal {
